7b91ee7eca04b953bc23f1474ca4a9364e70d9fa
Seolhee Kang <seolheekang1221@gmail.com>
2022-02-05 20:01:33 -0500

    text generator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import Container from './components/Container';
+import Title from './components/Title';
+import Options from './components/Options';
+import Output from './components/Output';
 
-function App() {
+const App = () => {
+  const [paragraphs, setParagraphs] = useState([]);
+  const [tag, setTag] = useState("p");
+  const [inputValue, setInputValue] = useState(1);
+  const [includeHtml, setIncludeHtml] = useState("Yes");
+  const [copiedCode, setCopiedCode] = useState(true);
+
+  useEffect(() => {
+    const url = 'https://baconipsum.com/api/?type=all-meat&paras=${inputValue}&start-with-lorem=1';
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setParagraphs(data));
+  }, [inputValue]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Title />
+        <Options
+        paragraphs={paragraphs}
+        copiedCode={copiedCode}
+        setCopiedCode={setCopiedCode}
+        setIncludeHtml={setIncludeHtml}
+        includeHtml={includeHtml}
+        setInputValue={setInputValue}
+        tag={tag}
+        setTag={setTag}
+        />
+        <Output paragraphs={paragraphs} includeHtml={includeHtml} tag={tag} />
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
